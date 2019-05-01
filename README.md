d1e3371d2d7e6ae9f153a50209c78ce4ce436654
U-YE-AW15R3\Ye <wangyeee@gmail.com>
2019-05-01 16:06:28 +1200

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# DIYDummyLoad
+A primitive DC dummy load with STM32F103.
+
+## Components
+1. STM32F103RC MCU.
+2. LMV321 OpAmp and IRFB3607PBF N channel MOSFET for the load.
+3. LMV324 as input buffer for the STM32 ADC.
+3. ILI9163C LCD (Thanks to [Adafruit](https://www.adafruit.com) for the [GFX Library](https://github.com/adafruit/Adafruit-GFX-Library))
--- src/ui/ILI9163C.c
+++ src/ui/ILI9163C.c
@@ -1,3 +1,33 @@
+/*
+ILI9163C port of the Adafruit GFX Library on STM32F103
+
+Adafruit invests time and resources providing this open source code, please
+support Adafruit & open-source hardware by purchasing products from Adafruit!
+
+Copyright (c) 2013 Adafruit Industries.  All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "ILI9163C.h"
 #include "stm32f10x_gpio.h"
 #include "delay.h"
@@ -173,6 +203,7 @@ void ILI9163CInit(void) {
 
     delay_ms(1);
     clearScreen(BLACK);
+#if 0
     //////test////
     delay_ms(1000);
     clearScreen(RED);
@@ -183,6 +214,7 @@ void ILI9163CInit(void) {
     delay_ms(1000);
     clearScreen(WHITE);
     //////test////
+#endif
 
     setTextColorBg(BLACK, WHITE);
     setTextSize(1);
